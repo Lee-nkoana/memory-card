a013a7d6a9054b00a25e5c1e4fc23eaf1091684d
Lebohang Nkoana <lenkoanjhb024@student.wethinkcode.co.za>
2026-01-27 14:00:20 +0200

    created function to pull images from api

---
+++ src/components.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+
+function Cards(){
+    const [getPokemonData, setPokemonData] = useState([]);
+    const [getLoading, setLoading] = useState(true);
+    const [score, setScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
+
+    useEffect(() => {
+        async function fetchPokemonData() {
+            try {
+                const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+                const data = await response.json();
+
+                const details = await Promise.all(
+                    data.results.map(async (e) => {
+                        const res = await fetch(e.url);
+                        return res.json();
+                    })
+                );
+
+                setPokemonData(details);
+                setLoading(false);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchPokemonData();
+    })
+
+    function handleClick(){
+        console.log("Clicked")
+        setScore(score+1);
+        console.log(score);
+        if (score > highScore){
+        setHighScore(score);
+        }
+        console.log(highScore);
+    }
+
+    if (getLoading == true) return (<> <p> Data is still loading...</p> </>);
+
+    return (
+        <>
+            <Header score={score} highScore={highScore}/>
+            <div style = {{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap:"12px"}}>
+                {getPokemonData.map((e) => (
+                    <div key={e.id} style={{textAlign: "center"}} onClick={handleClick}>
+                        <img src={e.sprites.front_default} alt={e.name} />
+                        <p>{e.name}</p>
+                    </div>
+                ))}
+            </div>
+        </>
+            
+    );
+}
+
+function Header({ score, highScore }){
+    return(
+        <div className="headerDesign">
+            <h3>
+                Score: {score}
+            </h3>
+            <h3>
+                highScore: {highScore}
+            </h3>
+        </div>
+    )
+
+}
+
+// export {Cards, Header}
+export {Header, Cards}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './components.css'
+
 import App from './App.jsx'
+import { Header, Cards } from './components.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Cards />
   </StrictMode>,
 )
